74894ba1d4d7f98a6738874b0a9e7fbc58f89de9
nour-gamal <nmg181963@hotmail.com>
2022-05-11 01:45:42 +0200

    clean up

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -4,7 +4,6 @@ mongoose
 	.connect(MONGODB_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
-		useCreateIndex: true,
 	})
 	.then(() => {})
 	.catch((error) => console.log(`${error} did not connect`));

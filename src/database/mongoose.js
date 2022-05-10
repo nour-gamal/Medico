661d778403358930c2b624075bcf03195c1948d9
nour-gamal <nmg181963@hotmail.com>
2022-05-11 01:43:21 +0200

    clean up

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const MONGODB_URL = process.env.MONGODB_URL;
 mongoose
-	.connect(MONGODB_URL)
+	.connect(MONGODB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
 	.then(() => {})
 	.catch((error) => console.log(`${error} did not connect`));

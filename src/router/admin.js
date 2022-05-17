e49a895b44e072893bcf7d1083f2a8f896109975
nour-gamal <nmg181963@hotmail.com>
2022-05-18 00:15:24 +0200

    clean up

--- src/router/admin.js
+++ src/router/admin.js
@@ -2,20 +2,7 @@ const express = require("express");
 const Admin = require("../models/Admin");
 const adminRouter = new express.Router();
 
-adminRouter.post("/admin/add", async (req, res) => {
-	const newAdmin = new Admin(req.body);
-	try {
-		await newAdmin.save();
-		res
-			.status(201)
-			.send({
-				code: 201,
-				message: "Admin added successfully!",
-			});
-	} catch (error) {
-		res.status(400).send({ code: 400, message: error.message });
-	}
-});
+
 
 
 
--- src/router/common.js
+++ src/router/common.js
@@ -4,6 +4,41 @@ const Admin = require('../models/Admin')
 const auth = require("../middlewares/auth");
 
 
+commonRouter.post('/signup', async (req, res) => {
+    const { userType } = req.body;
+    var status = null;
+    try {
+        if (!userType) {
+            throw new Error("Please provide user type!")
+        }
+        switch (userType) {
+            case 1:
+                status = Admin.adminSignup(req.body);
+                console.log(status.response)
+                if (status !== 1) {
+                    throw new Error(status.message)
+                }
+                break;
+            default:
+                throw new Error("Invalid user type!")
+                break;
+        }
+        const userTypeName = userType === 1 ? 'Admin' : userType === 2 ? 'Doctor' : 'Patient'
+        if (status) {
+            res
+                .status(201)
+                .send({
+                    code: 201,
+                    message: `${userTypeName} successfully Registered!`,
+                });
+        }
+    } catch (error) {
+        res.status(400).send({ code: 400, message: error.message })
+    }
+
+
+})
+
 commonRouter.post('/signin', async (req, res) => {
     const { email, password, userType } = req.body;
     const isValidParams = !email || !password || !userType;

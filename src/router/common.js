52d3f466538ecb571478bf7ff4b762bc80e8530f
nour-gamal <nmg181963@hotmail.com>
2022-05-20 23:13:06 +0200

    clean up

--- src/router/common.js
+++ src/router/common.js
@@ -35,38 +35,7 @@ commonRouter.post('/signup', async (req, res) => {
     } catch (error) {
         res.status(400).send({ code: 400, message: error.message })
     }
-
-    commonRouter.post('/signup', async (req, res) => {
-        const { userType } = req.body;
-        var status = null;
-        try {
-            if (!userType) {
-                throw new Error("Please provide user type!")
-            }
-            switch (userType) {
-                case 1:
-                    status = Admin.adminSignup(req.body);
-                    console.log(status.response)
-                    if (status !== 1) {
-                        throw new Error(status.message)
-                    }
-                    break;
-                default:
-                    throw new Error("Invalid user type!")
-                    break;
-            }
-            const userTypeName = userType === 1 ? 'Admin' : userType === 2 ? 'Doctor' : 'Patient'
-            if (status) {
-                res
-                    .status(201)
-                    .send({
-                        code: 201,
-                        message: `${userTypeName} successfully Registered!`,
-                    });
-            }
-        } catch (error) {
-            res.status(400).send({ code: 400, message: error.message })
-        }
+})
 
 
 commonRouter.post('/signin', async (req, res) => {
@@ -86,19 +55,19 @@ commonRouter.post('/signin', async (req, res) => {
                 break;
             default:
                 throw new Error()
-            }
-            switch (userType) {
-                case 1:
-                    user = await Admin.adminSignin(email, password)
-                    break;
-                default:
-                    throw new Error()
-                    break;
-            }
-            res.status(200).send({ code: 200, data: user })
-        } catch (error) {
-            res.status(400).send({ code: 400, message: 'Invalid login attempt!' })
         }
-    })
+        switch (userType) {
+            case 1:
+                user = await Admin.adminSignin(email, password)
+                break;
+            default:
+                throw new Error()
+                break;
+        }
+        res.status(200).send({ code: 200, data: user })
+    } catch (error) {
+        res.status(400).send({ code: 400, message: 'Invalid login attempt!' })
+    }
+})
 
-    module.exports = commonRouter;
+module.exports = commonRouter;

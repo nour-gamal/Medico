bb3c525910247f770d6c1b5aa9195e85ff61349b
nour-gamal <nmg181963@hotmail.com>
2022-06-01 23:47:35 +0200

    add CRUD Operation for admins

--- src/router/admin.js
+++ src/router/admin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Admin = require("../models/Admin");
+const User = require('../models/User')
 const auth = require("../middlewares/auth");
 const adminRouter = new express.Router();
 const verifiedRoles = require("../middlewares/verified_Roles");
@@ -18,6 +19,24 @@ adminRouter.get('/getAllAdmins', auth, verifiedRoles('Super Admin'), async (req,
     }
 })
 
-
-
+adminRouter.patch('/editAdmin', auth, verifiedRoles('Super Admin'), async (req, res) => {
+    try {
+        const { _id, ...data } = req.body
+        await Admin.findByIdAndUpdate(_id, data, { new: true })
+        await User.findByIdAndUpdate(_id, data, { new: true })
+        res.status(200).send({ code: 200, message: 'Admin updated successfully!' })
+    } catch (error) {
+        res.status(400).send({ code: 400, message: error.message })
+    }
+})
+adminRouter.delete('/deleteAdmin', auth, verifiedRoles('Super Admin'), async (req, res) => {
+    try {
+        const { _id } = req.body
+        await Admin.findByIdAndDelete(_id);
+        await User.findByIdAndDelete(_id);
+        res.status(200).send({ code: 200, message: 'Admin deleted successfully!' })
+    } catch (error) {
+        res.status(400).send({ code: 400, message: error.message })
+    }
+})
 module.exports = adminRouter;

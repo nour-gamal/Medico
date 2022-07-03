b67b6197eca16bb52a2117530ff2aaea49e96d55
nour-gamal <nmg181963@hotmail.com>
2022-07-03 21:20:38 +0200

    clean up

--- src/middlewares/verified_Roles.js
+++ src/middlewares/verified_Roles.js
@@ -2,16 +2,16 @@ const verifiedRoles = (allowedRoles) => {
     return (req, res, next) => {
         const AdminRole = req.user.role ? req.user.role : req.userType === 2 ? "Doctor" : "Patient";
         try {
-            // if (!AdminRole) {
-            //     throw new Error()
-            // }
-            // if (!allowedRoles.includes(AdminRole.toString())) {
-            //     throw new Error()
-            // }
+            if (!AdminRole) {
+                throw new Error('no role')
+            }
+            if (!allowedRoles.includes(AdminRole.toString())) {
+                throw new Error('role not')
+            }
 
             next();
         } catch (error) {
-            res.status(401).send({ code: 401, message: 'You are not authorized to perform this action!' })
+            res.status(401).send({ code: 401, message: error.message })
         }
     }
 }
